bef9c83f7dc93a1ad303900a460f1e446b02aef7
Iman <iman.od@gmail.com>
2023-01-23 22:11:08 -0600

    adding features file

--- src/features/create_features.py
+++ src/features/create_features.py
@@ -1,10 +1,12 @@
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-iris_data = pd.read_csv('../data/iris.data', \
+
+# Reading the raw data file
+iris_data = pd.read_csv('../../data/raw/iris.data', \
     names = [ 'sepal_length_cm','sepal_width_cm','petal_length_cm','petal_width_cm','class'])
 
 
 
 # write the features file
-iris_data.to_csv('../features','\t')
+iris_data[['sepal_length_cm','sepal_width_cm','class']].to_csv('../../data/processed/features.csv','\t')
 
